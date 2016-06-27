470d6c3d4d47c333e0e27a6197cb075837fb72f1
ronak5279 <ronak5279.agarwal@gmail.com>
2016-06-28 05:03:26 +0530

    11th commit tut 2

--- admin.py
+++ admin.py
@@ -8,7 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-  
-	list_display = ('question_text', 'pub_date', 'was_published_recently')
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+	
 admin.site.register(Question, QuestionAdmin)
